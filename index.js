b66b0c77be1b4a9023090da665d7dd8134d95020
Johannes J. Schmidt <schmidt@netzmerk.com>
2013-01-28 16:44:30 +0100

    encode id in tempfile path
    thanks @semanticdreamer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function generateStills(doc, name, version, options, done) {
         + '/' + encodeURIComponent(doc._id)
         + '/' + encodeURIComponent(name),
       basename = name.replace(/\..*$/, ''),
-      prefix = '/tmp/' + doc._id + '-' + version  + '-' + basename.replace('/', '-') + '-',
+      prefix = '/tmp/' + encodeURIComponent(doc._id) + '-' + version  + '-' + basename.replace('/', '-') + '-',
       suffix = '.jpg',
       args = ['-i', '-', '-r', options.ratio || '1/10', prefix + '%04d' + suffix],
       ffmpeg = spawn('ffmpeg', args);
